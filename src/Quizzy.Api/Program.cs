246f346142f11ff6f5927b66b11a36f1391d0190
Edmon <ed.moussa4@gmail.com>
2026-03-04 16:39:30 +0300

    Added Identity and invoked RoleSeeder

--- src/Quizzy.Api/Program.cs
+++ src/Quizzy.Api/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Quizzy.Api.Data;
+using Quizzy.Api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,27 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options =>
+{
+    // Password settings
+    options.Password.RequiredLength = 8;
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+
+    // Lockout settings
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.MaxFailedAccessAttempts = 5;
+    options.Lockout.AllowedForNewUsers = true;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+// Seed fixed roles (Admin, Teacher, Student)
+await RoleSeeder.SeedAsync(app.Services);
 
 if (app.Environment.IsDevelopment())
 {
