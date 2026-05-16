578440a4799b08e416a2a98b88ba39ae6c313677
EdmonGM <ed.moussa4@gmail.com>
2026-05-16 17:11:23 +0300

    Increased Quiz description size

---
+++ src/Quizzy.Api/Migrations/20260516125018_IncreaseQuizDescriptionMaxLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quizzy.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseQuizDescriptionMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Quizzes",
+                type: "character varying(512)",
+                maxLength: 512,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(128)",
+                oldMaxLength: 128);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Quizzes",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(512)",
+                oldMaxLength: 512);
+        }
+    }
+}
