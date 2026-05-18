d1bef1c558fce2c4735640ee027d76c383946613
EdmonGM <ed.moussa4@gmail.com>
2026-05-18 15:13:00 +0300

    Centralized app roles

--- src/Quizzy.Api/Controllers/AccountsController.cs
+++ src/Quizzy.Api/Controllers/AccountsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Quizzy.Api.Constants;
 using Quizzy.Api.Data;
 using Quizzy.Api.Dtos;
 using Quizzy.Api.Mappers;
@@ -26,7 +27,6 @@ public class AccountsController(
     ILogger<AccountsController> logger)
     : ControllerBase
 {
-    private const string RoleAdmin = "Admin";
 
     /// <summary>
     /// Retrieves a user by their unique identifier.
@@ -89,7 +89,7 @@ public class AccountsController(
     /// <response code="401">If the request is not authenticated.</response>
     /// <response code="403">If the current user does not have Admin role.</response>
     [HttpGet]
-    [Authorize(Roles = RoleAdmin)]
+    [Authorize(Roles = AppRoles.Admin)]
     [ProducesResponseType(typeof(List<UserResponseDto>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
